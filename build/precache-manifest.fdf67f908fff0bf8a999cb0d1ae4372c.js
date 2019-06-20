366fdf4d063d72ae0e86e093ea0896f37365613b
scratch342 <scratchcat342@gmail.com>
2019-06-20 23:11:03 +0930

    commit 1

---
+++ build/precache-manifest.fdf67f908fff0bf8a999cb0d1ae4372c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20dcd4a84378d9dca27d4e9f69325af0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccacf2a83e88e7de15dc",
+    "url": "/static/css/main.a98fcbbd.chunk.css"
+  },
+  {
+    "revision": "8305ae5356b03c47a4ed",
+    "url": "/static/js/2.7a937d88.chunk.js"
+  },
+  {
+    "revision": "ccacf2a83e88e7de15dc",
+    "url": "/static/js/main.f23456e8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
